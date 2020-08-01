16788d2263d8ca0b57272938728b80fe885fe11d
lucas <lucasmartinez.it@gmail.com>
2020-08-01 10:26:40 +0200

    Updates

---
+++ precache-manifest.b1c17c9ec58606be577f3c339c0fb56e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f545c33a73e6509e590bae2d1c9f44",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a949f0f128f7b4046128",
+    "url": "/monsters-rolodex/static/css/main.90d7bbf6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a949f0f128f7b4046128",
+    "url": "/monsters-rolodex/static/js/main.a08d3606.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
